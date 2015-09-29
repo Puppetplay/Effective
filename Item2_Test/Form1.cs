ca2d07fb17f0f01ab046d2f92255679c5becb681
이창민 <airkiss81@gmail.com>
2015-09-30 02:47:33 +0900

    Item2

---
+++ Item2_Test/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Item2_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Effective.Item2._3_값확인.ConstValue.ToString();
+            textBox2.Text = Effective.Item2._3_값확인.ReaonlyValue.ToString();
+        }
+    }
+}
