55dafbf2386f2c72bc2f1351e7dc5e35192d66d1
이창민 <airkiss81@gmail.com>
2015-11-03 23:46:09 +0900

    Item23

---
+++ Effective03/Item23/1_MyBusinessObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Effective03.Item23
+{
+    class MyBusinessObject
+    {
+        private DataSet _ds;
+        public DataSet Data
+        {
+            get { return _ds; }
+        }
+    }
+
+
+    public class TestClass01
+    {
+        public void Func1()
+        {
+            MyBusinessObject bo = new MyBusinessObject();
+            DataSet ds = bo.Data;
+            ds.Tables.Clear();
+        }
+    }
+}
---
+++ Effective03/Item23/3_DataViewManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Effective03.Item23
+{
+    public class MyBusinessObject2
+    {
+        private DataSet _ds;
+        public DataView this[string tableName]
+        {
+            get
+            {
+                return _ds.DefaultViewManager.CreateDataView(_ds.Tables[tableName]);
+            }
+        }
+    }
+
+    public class TestClass02
+    {
+        public void Func1()
+        {
+            MyBusinessObject2 mo = new MyBusinessObject2();
+            DataView v = mo["customers"];
+            foreach(DataRowView r in v)
+            {
+                Console.WriteLine(r["name"]);
+            }
+        }
+    }
+}
