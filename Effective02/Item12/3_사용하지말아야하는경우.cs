2e4fa77f2c2adf96b90b54c76fb94da73cf030ca
이창민 <airkiss81@gmail.com>
2015-11-10 22:32:26 +0900

    1

--- Effective02/Item12/3_사용하지말아야하는경우.cs
+++ Effective02/Item12/3_사용하지말아야하는경우.cs
@@ -9,31 +9,31 @@ namespace Effective02.Item12
 {
     class _3_사용하지말아야하는경우
     {
-        int number = 0;
-        String myString = null;
+        //int number = 0;
+        //String myString = null;
 
-        private ArrayList arrayList = new 효();
+        //private ArrayList arrayList = new 효();
 
-        public _3_사용하지말아야하는경우()
-        {
+        //public _3_사용하지말아야하는경우()
+        //{
             
-        }
+        //}
 
-        public _3_사용하지말아야하는경우(int size)
-        {
-            arrayList = new ArrayList(size);
-        }
+        //public _3_사용하지말아야하는경우(int size)
+        //{
+        //    arrayList = new ArrayList(size);
+        //}
 
-        public _3_사용하지말아야하는경우(string str)
-        {
-            try
-            {
-                arrayList = new ArrayList(Int32.Parse(str));
-            }
-            catch
-            {
-                arrayList = new ArrayList();
-            }
-        }
+        //public _3_사용하지말아야하는경우(string str)
+        //{
+        //    try
+        //    {
+        //        arrayList = new ArrayList(Int32.Parse(str));
+        //    }
+        //    catch
+        //    {
+        //        arrayList = new ArrayList();
+        //    }
+        //}
     }
 }
